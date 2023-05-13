ba698529f669c3cf95d72b7ad75296d2e75d641b
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-13 16:36:06 -0300

    caracter alfanumerico - ASCII

--- 02_Estructura Decision/02_Ejercicios/09_ejercicio.py
+++ 02_Estructura Decision/02_Ejercicios/09_ejercicio.py
@@ -1,1 +1,12 @@
-# 9
+# 9 Confeccionar un programa que permita ingresar un caracter alfanumerico y determine e informe si lo ingresado corresponde a una vocal, con el mensaje “VOCAL”. y su correspondiente valor numérico en ASCII.
+caracter = input("Ingrese un carácter alfanumérico: ")
+
+if len(caracter) == 1:  # Verificar que se haya ingresado un solo carácter
+    if caracter in "AEIOUaeiou":  # Verifica si el carácter ingresado es una vocal
+        print("VOCAL")
+    else:
+        print("NO VOCAL")
+    # Imprimir el valor numérico en ASCII del carácter ingresado
+    print("Valor en ASCII: ", ord(caracter))
+else:
+    print("Debe ingresar un solo carácter alfanumérico.")
