0d371f084df6941cf44c39f97a5e503eccfad7e8
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-14 00:01:42 -0300

    Sintaxis de listas

--- 03_Iteracion/01_Ejercicios/00_listas.py
+++ 03_Iteracion/01_Ejercicios/00_listas.py
@@ -7,3 +7,14 @@ for valor in range(1, 5):  # "0, 1, 2, 3, 4, 5"
 # Si se coloca Range() dentro de un list() te los guarda en una lista
 numero = list(range(1, 5))  # [0, 1, 2, 3, 4, 5]
 print(numero)
+
+# Uso de listas
+# 1.0 Cortar una lista
+jugadores = ["charles", "martina", "martin", "franco", "facundo"]
+print(jugadores[0:3])  # [posicion:elemento] o [elemento:] o [:elemento]
+
+# 1.1 Copiar una lista
+lista_nombres1 = ["mario", "miguel", "franco", "mauro", "facundo"]
+lista_nombres2 = ["antonella", "maria", "malena", "javier", "leo"]
+print(lista_nombres1)
+print(lista_nombres2)
