ec03b68a45bb23dc030ec24c1ec7ae7e3cd3bb70
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-13 23:49:30 -0300

    Sintaxis - Iteracion

--- 03_Iteracion/01_Ejercicios/00_listas.py
+++ 03_Iteracion/01_Ejercicios/00_listas.py
@@ -1,1 +1,9 @@
-#
+# Range() genera una secuencia // (2 donde empieza , 11 donde termina, 2 tamaño de paso al generar números)
+for valor in range(2, 11, 2):  # va de 2 en 2
+    print(valor)
+for valor in range(1, 5):  # "0, 1, 2, 3, 4, 5"
+    print(valor)
+
+# Si se coloca Range() dentro de un list() te los guarda en una lista
+numero = list(range(1, 5))  # [0, 1, 2, 3, 4, 5]
+print(numero)
