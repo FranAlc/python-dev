df82a9bc4d499809873b578cff22b965503a1cb4
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-15 19:58:03 -0300

    Ejercicio de Iteracion - D - E

--- 03_Iteracion/01_Ejercicios/01_ejercicio.py
+++ 03_Iteracion/01_Ejercicios/01_ejercicio.py
@@ -1,1 +1,5 @@
-# 01
+# 01 Mostrar por pantalla los números pares comprendidos entre 100 y 500 dentro de una lista.
+
+numeros = list(range(100, 501, 2))
+
+print(f"{numeros}")
---
+++ 03_Iteracion/01_Ejercicios/04_ejercicio.py
@@ -0,0 +1,9 @@
+# 4 Se ingresan 10 letras. Determinar la cantidad de vocales ingresadas.
+letras = input("Ingresa 10 letras: \n")
+cantidad_vocales = 0
+
+for letra in letras:
+    if letra.lower() in 'aeiouAEIOU':
+        cantidad_vocales += 1
+
+print(f"La cantidad de vocales mencionadas es: {cantidad_vocales}")
