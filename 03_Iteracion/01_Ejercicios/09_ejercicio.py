20dd43c5e5db0da57feff49fe106b17c3a9d7430
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-16 12:34:03 -0300

    Ejercicio de Iteracion

--- 03_Iteracion/01_Ejercicios/09_ejercicio.py
+++ 03_Iteracion/01_Ejercicios/09_ejercicio.py
@@ -1,1 +1,9 @@
 # 09 Confeccionar un programa que exhiba por pantalla una lista, a dos columnas, con los primeros 15 números impares en la primera y los 15 primeros pares en la segunda, incluyendo los títulos.
+
+print("Números impares   Números pares")
+print("----------------  --------------")
+
+for i in range(1, 16):
+    num_impar = 2 * i - 1
+    num_par = 2 * i
+print("{:^16d}  {:^16d}".format(num_impar, num_par))
