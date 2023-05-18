fe68394c5aea965719157b3f832f5b4271b91739
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-17 23:17:56 -0300

    Algunos metodos basicos

--- 03_Iteracion/01_Ejercicios/00_metodos.py
+++ 03_Iteracion/01_Ejercicios/00_metodos.py
@@ -5,17 +5,20 @@
 nm = "franco"
 apl = "galan"
 print("{} {}".format(nm.title(), apl.title()))
-# Metodo minuscula
-nm = "Franco"
-print(nm.lower())
-# Metodo espacios en blanco -> rstrip()
-nm, apl = "Franco", "Galan"
-print(nm.rstrip())
 
 # Metodo mayuscula primera letra
 nm = "franco"
 print(nm.title())
+
 # Concatenacion con el metodo title
 nm = "franco"
 apl = "galan"
 print(f"Hola, {nm.title()} {apl.title()} ")
+
+# Metodo minuscula
+nm = "Franco"
+print(nm.lower())
+
+# Metodo espacios en blanco -> rstrip()
+nm, apl = "Franco", "Galan"
+print(nm.rstrip())
