894dde915e770e3e73943fdac56eed41ec4d6602
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-19 21:42:47 -0300

    Ejercicios Estructura Secuencial

--- 01_Estructura Secuencial/02_Ejercicios/02_ejercicio.py
+++
@@ -1,8 +0,0 @@
-# 0.2 Se ingresan las notas de dos evaluaciones de un alumno. Determinar la nota promedio
-
-nota_evaluacion1 = float(input("Ingresa la primer nota: "))
-nota_evaluacion2 = float(input("Ingresa la segunda nota: "))
-
-suma_notas = (nota_evaluacion1 + nota_evaluacion2) / 2
-
-print(f"La nota promedio: {suma_notas}")
