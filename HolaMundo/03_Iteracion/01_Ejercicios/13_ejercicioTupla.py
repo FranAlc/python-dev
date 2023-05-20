bec7c42e84f652201227885c5c72ab6a49e30064
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-19 23:47:32 -0300

    Aprendizaje de tuplas

---
+++ HolaMundo/03_Iteracion/01_Ejercicios/13_ejercicioTupla.py
@@ -0,0 +1,8 @@
+# Dimension normal
+# dimensiones=(200,50)
+# print("Dimensiones originales: ")
+# for dimension in dimensiones: print(dimension)
+# Dimension modificada
+# dimensiones=(400,100)
+# print("Dimensiones modificadas: ")
+# for dimension in dimensiones: print(dimension)
