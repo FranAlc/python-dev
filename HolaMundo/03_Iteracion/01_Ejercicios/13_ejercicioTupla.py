a3364b3b2de557c9f4c05bc90cacb0090202befe
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-20 04:51:06 -0300

    Ejercicio con uso de tupla

--- HolaMundo/03_Iteracion/01_Ejercicios/13_ejercicioTupla.py
+++ HolaMundo/03_Iteracion/01_Ejercicios/13_ejercicioTupla.py
@@ -6,14 +6,12 @@
 # dimensiones=(400,100)
 # print("Dimensiones modificadas: ")
 # for dimension in dimensiones: print(dimension)
-
 """
 13. Buffet: Un restaurante estilo buffet ofrece solo cinco alimentos básicos. Piense en cinco
 alimentos simples y guárdelos en una tupla.
 -Utilice un bucle for para imprimir cada comida que ofrece el restaurante.
--Intente modificar uno de los elementos y asegúrese de que Python rechace el cambio
+-Intente modificar uno de los elementos y asegúrese de que Python rechace el cambio.
 -El restaurante cambia su menú, reemplazando dos de los artículos con alimentos diferentes.
 Agregue una línea que reescriba la tupla y luego use un ciclo for para imprimir cada uno
-de los elementos en el menú revisado.
-
+de los elementos en el menu revisado.
 """
