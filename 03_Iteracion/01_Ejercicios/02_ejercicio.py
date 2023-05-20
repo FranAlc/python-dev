0e521dc2477ef7f810d5a01807c6ac5e0652dc7c
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-19 21:45:47 -0300

    Ejercicios de Iteracion

--- 03_Iteracion/01_Ejercicios/02_ejercicio.py
+++
@@ -1,7 +0,0 @@
-# 02_ Confeccionar un programa para calcular el valor de la siguiente suma: 100 + 95 + 90 + 50 + ...
-
-numero = int(input("Ingresa un numero a la suma: "))
-
-resultado = 100 + 95 + 90 + 50 + numero
-
-print("El resultado:", resultado)
--- 03_Iteracion/01_Ejercicios/05_ejercicio.py
+++
@@ -1,6 +0,0 @@
-# 05 Confeccionar un programa para calcular la suma de los primeros N números naturales. El valor de N lo solicita por teclado el programa.
-n = int(input("Ingrese un número entero positivo: "))
-
-suma = n * (n + 1) // 2  # No es necesario usar un bucle
-
-print(f"La suma de los primeros {n} números naturales es: {suma}")
