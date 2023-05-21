c66ca2615235cf31f5c6b31e383d90cabfd96723
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-21 15:06:44 -0300

    Ejercicio basico de edad

---
+++ HolaMundo/02_Estructura Decision/Ejercicios/11-ejercicio.py
@@ -0,0 +1,8 @@
+# 11 Realizar un sistema que valide si eres mayor o menor de edad - 18 o + sos mayor , sino es menor
+
+edad = int(input("Ingresa tu edad: "))
+
+if edad >= 18:
+    print("Sos mayor")
+else:
+    print("Sos menor")
