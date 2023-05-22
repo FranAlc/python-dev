d6f719e8637ca7efcf1309156c0f302195acd616
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-22 15:04:25 -0300

    Ejercicio de funcion .upper()

--- CISCO-Py/00_datos.py
+++ CISCO-Py/00_datos.py
@@ -1,3 +1,4 @@
+"""
 # break - ejemplo
 print("La instrucción break:")
 for i in range(1, 6):
@@ -13,3 +14,15 @@ for i in range(1, 6):
         continue
     print("Dentro del bucle.", i)
 print("Fuera del bucle.")
+"""
+c0 = int(input("ingresa un numero que no sea negativo y que no sea cero: "))
+while c0 > 0 and c0 != 0:
+
+    resultado = c0/2 == 0
+    if (resultado == 0):
+        print(f"El numero {c0} es par")
+
+    elif (resultado != 0):
+        print(f"El numero {c0} es impar")
+
+    c0 = int(input("ingresa un numero que no sea negativo y que no sea cero: "))
--- CISCO-Py/01_ejerciciosCisco.py
+++ CISCO-Py/01_ejerciciosCisco.py
@@ -15,41 +15,19 @@ while (frase_user != "chupacabra"):
 print("Has dejado el bucle con exito.")
 
 ## ------------------------------------------------------##
-# 3.0 Tu programa debe:
-"""
-Pedir al usuario que ingrese una palabra.
-utiliza user_word = user_word.upper() para convertir la palabra ingresada por el usuario a mayúsculas; hablaremos sobre los llamados métodos de cadena y el método upper() muy pronto, no te preocupes
-utiliza la ejecución condicional y la instrucción continue para "devorar" las siguientes vocales A, E, I, O, U de la palabra ingresada.
-imprime las letras no consumidas en la pantalla, cada una de ellas en una línea separada
-Prueba tu programa con los datos que le proporcionamos.
-"""
 
-palabra = input("Ingresa una palabra: ")  # ingresa palabra
-print(palabra.upper())  # convertir palabra ingresada a mayuscula
-
-resultado = ""
-for letra in palabra:
-    if letra.upper() in ['A', 'E', 'I', 'O', 'U']:
-        continue  # Salta a la siguiente iteración del bucle si es una vocal
-    resultado += letra
-print("Letras vocales consumidas : ", letra.upper())
 
 ## ------------------------------------------------------##
 
-# 4.0 En 1937, un matemático alemán llamado Lothar Collatz formuló una hipótesis intrigante (aún no se ha comprobado) que se puede describir de la siguiente manera:
-"""
-1.toma cualquier número entero que no sea negativo y que no sea cero y asígnale el nombre c0;
-2.si es par, evalúa un nuevo c0 como c0 ÷ 2;
-3.de lo contrario, si es impar, evalúe un nuevo  c0  como 3 x c0 + 1;
-4.si c0 ≠ 1, salta al punto 2.
-La hipótesis dice que, independientemente del valor inicial de c0, el valor siempre tiende a 1.
-Escribe un programa que lea un número natural y ejecute los pasos anteriores siempre que c0 sea diferente de 1. También queremos que cuente los pasos necesarios para lograr el objetivo. Tu código también debe mostrar todos los valores intermedios de c0.
-"""
+
 c0 = int(input("ingresa un numero que no sea negativo y que no sea cero: "))
 while c0 > 0 and c0 != 0:
+
     resultado = c0/2 == 0
-    if (resultado == 0):
-        print(f"El numero {c0} es par.")
     if (resultado != 0):
+        print(f"El numero {c0} es par.")
+        break
+    elif (resultado == 0):
         print(f"{c0} es impar")
+        break
 c0 = int(input("ingresa un numero que no sea negativo y que no sea cero: "))
