1dcc09560c9482fb80a35084688759f60726143d
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-22 04:50:13 -0300

    Ejercicios iteracion for

--- CISCO-Py/01_ejerciciosCisco.py
+++ CISCO-Py/01_ejerciciosCisco.py
@@ -4,12 +4,12 @@ for i in range(1, 6):
 print("¡Listos o no aquí voy!")
 
 # 2.0 Diseña un programa que use un bucle while y le pida continuamente al usuario que ingrese una palabra a menos que ingrese "chupacabra" como la palabra de output secreta, en cuyo caso el mensaje "Has dejado el bucle con éxito." debe imprimirse en la pantalla y el bucle debe terminar.
-frase_user = input("ingresa una palabra:")
+frase_user = input("ingresa una palabra: ")
 
 while (frase_user != "chupacabra"):
 
     print("Estas dentro del bucle!")
-    frase_user = input("ingresa una palabra")
+    frase_user = input("ingresa una palabra: ")
 print("Has dejado el bucle con exito.")
 
 # 3.0 Tu programa debe:
@@ -20,3 +20,6 @@ utiliza la ejecución condicional y la instrucción continue para "devorar" las 
 imprime las letras no consumidas en la pantalla, cada una de ellas en una línea separada
 Prueba tu programa con los datos que le proporcionamos.
 """
+
+palabra = input("Ingresa una palabra: ")  # ingresa palabra
+print(palabra.upper())  # convertir palabra ingresada a mayuscula
