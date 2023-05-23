2d77ff2ef3d411190952c0219a892e2c9a5803c6
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-23 16:07:32 -0300

    Ejercicio promedio de numeros

---
+++ HolaMundo/Practica/01_ejercicioPractica.py
@@ -0,0 +1,20 @@
+# 01 Ejercicio promedio de dos numeros
+
+# entrada
+num1 = int(input("Ingresa un valor: "))
+num2 = int(input("Ingresa un segundo valor: "))
+
+# proceso
+s = num1 + num2
+p = s/2
+
+# salida
+print("-------------------------------------")
+print("Ejercicio 1: Promedio de dos numeros")
+print("-------------------------------------")
+print("Ingrese notas:")
+print(f"N1: {num1}")
+print(f"N2: {num2}")
+print("\nSalida:")
+print("-------------------------------------")
+print("Promedio: {}".format(p))
