e60f87168aba84e627d8abe6c76862462d055346
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-23 22:46:08 -0300

    Uso de listas y libreria math

--- HolaMundo/Practica/14_ejercicioPractica.py
+++ HolaMundo/Practica/14_ejercicioPractica.py
@@ -1,10 +1,11 @@
 # 14 Determine la distancia entre dos puntos en el espacio.
 # (x1,y1, z1) y (x2, y2, z2)
 
+import math
 punto_a = []
 punto_b = []
 
-for i in range(0, 6):
+for i in range(1):
     x1 = float(input("Digite el valor de x1: "))
     y1 = float(input("Digite el valor de y1: "))
     z1 = float(input("Digite el valor de z1: "))
@@ -21,3 +22,15 @@ for i in range(0, 6):
     punto_b.append(round(z2))
     print(f"Primer punto {punto_a}")
     print(f"Segundo punto {punto_b}")
+
+    d = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+    print("--------------------------------------------------")
+    print("Ejercicio 14: Ejercicio - Distancia de dos puntos")
+    print("--------------------------------------------------")
+    print("Datos ingresados:")
+    print(f"Lista primer punto: {punto_a}")
+    print(f"Lista segundo punto: {punto_b}")
+    print("\nSalida:")
+    print("--------------------------------------------------")
+    print(f"La distancia es: {d}")
+    print("--------------------------------------------------")
