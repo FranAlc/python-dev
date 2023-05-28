7a1a375b727ca3c81f28eabae3add0dc3c7f0e8b
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-28 07:25:43 -0300

    verifica si un numero es par o impar (2ej)

--- HolaMundo/Practica/23_ejercicioPractica.py
+++ HolaMundo/Practica/23_ejercicioPractica.py
@@ -1,10 +1,23 @@
 # 23 Se desea leer por teclado un número comprendido entre 0 y 10 (inclusive) y se desea visualizar si el número es par o impar.
-
+"""
 num = float(input("Numero entre 0 y 10: "))
 
 if num <= 10 and num >= 0:
     r = (num % 2) == 0
     if (r == 0):
+        print(f"El {round(num)} es impar")
+    else:
+        print(f"El {round(num)} es par")
+else:
+    print("El valor no coincide entre 0 y 10.")
+"""
+# otra forma:
+
+num = float(input("Numero entre 0 y 10: "))
+
+if num <= 10 and num >= 0:
+    r = (num % 2) == 0
+    if not r == 0:
         print(f"El {round(num)} es par")
     else:
         print(f"El {round(num)} es impar")
