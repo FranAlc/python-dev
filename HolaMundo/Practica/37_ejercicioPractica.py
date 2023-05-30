aedc37ffeb29185684b757df302cb5f1c7f434f3
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-30 16:02:49 -0300

    retornar funciones

---
+++ HolaMundo/Practica/37_ejercicioPractica.py
@@ -0,0 +1,11 @@
+def saludar():
+    def mostrar_mensaje():
+        print("Hola, estoy aprendiendo python y haciendo muchos ejercicios")
+
+    return mostrar_mensaje
+
+
+respuesta = saludar()
+
+respuesta()
+# print (respuesta) >> es una funcion y se retorna como arriba
