5ce05c81dd1cb6fc3e13a943fdca52f0b5b08674
Franco Galan <alcaidefranco.07@gmail.com>
2023-05-31 04:32:17 -0300

    Testeo de funciones

---
+++ HolaMundo/Practica/41_ejercicioPractica.py
@@ -0,0 +1,25 @@
+# Docstring
+# Testear funciones
+"""
+Para testear se necesitan 3 comillas dobles.
+Se llama a la funcion y se muestra aquello que realiza ejemplo :
+#Usamos -m doctest
+"""
+
+
+def suma(num1, num2):
+    """
+    Funcion suma 2 numeros enteros.
+    Argumentos:
+    num1 (int)
+    num2 (int)
+
+    Se retorna la suma de los parametros mencionados:
+    >>> suma(10,30)
+    50
+    >>> suma(50,50)
+    100
+
+    En caso que el resultado mencionado anteriormente no se cumpla saldra un error (linea 19)
+    """
+    return num1 + num2
