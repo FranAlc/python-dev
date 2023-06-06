93519d948a9b22839643badbfd425192feb1a2bc
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-06 15:06:37 -0300

    numero menor en lista

---
+++ HolaMundo/04_Funciones/Codificacion/14_EjercicioFunciones.py
@@ -0,0 +1,17 @@
+"""
+Digitar una funcion llamada min_in_list() que devuelva el numero menor dentro de una lista nombrada en el "main".
+"""
+
+# funcion
+
+
+def min_in_list(lista):
+    numero_mayor = min(lista)
+    return numero_mayor
+
+
+# main
+lista_de_numeros = [1, 2, 3, 4, 5, 6, 8, 58, 96, 78, 51]
+
+numero_max = min_in_list(lista_de_numeros)
+print(f"Numero menor: {numero_max}")
