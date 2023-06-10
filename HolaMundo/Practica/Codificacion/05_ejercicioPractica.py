8327cc4d53f0e78a77ba169770da36f28d44930c
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-10 07:01:56 -0300

    ejercicio radio de un circulo

---
+++ HolaMundo/Practica/Codificacion/05_ejercicioPractica.py
@@ -0,0 +1,16 @@
+# 05 Ejercicio radio de un circulo
+
+# Inicio
+pi = float(3.1416)
+radio = float((input("Digite el radio del circulo: ")))
+# Proceso
+v = 4/3 * pi * radio ** 3
+# Salida
+print("-------------------------------------")
+print("Ejercicio 5: Circulo")
+print("-------------------------------------")
+print("Dato ingresado:")
+print(f"Radio: {radio}")
+print("\nSalida:")
+print("-------------------------------------")
+print("Volumen de la esfera: {}".format(v))
