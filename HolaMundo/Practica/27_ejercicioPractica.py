3ffb232faee1d8b0ca7d425f904919619c48b174
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-10 06:54:06 -0300

    redireccionar ejercicios a una nueva carpeta

--- HolaMundo/Practica/27_ejercicioPractica.py
+++
@@ -1,8 +0,0 @@
-# 27 Dada una cadena que consta de palabras y espacios, devuelve la longitud de la última palabra de la cadena.
-# Una palabra es un máximo subcadena que consta únicamente de caracteres que no son espacios.
-
-palabra = "Argentina, Mexico, Uruguay, Canada"
-
-print(f"Cantidad de letras: {len(palabra)}")
-print(f"Palabra: {palabra}")
-print(f"Ultima palabra: {palabra[-6:]}")
