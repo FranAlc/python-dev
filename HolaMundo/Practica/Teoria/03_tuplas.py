3b15266a6642fd39bdc7812d48842c34a4d4e35f
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-12 09:33:49 -0300

    Anotación de Tuplas

---
+++ HolaMundo/Practica/Teoria/03_tuplas.py
@@ -0,0 +1,14 @@
+# 3. Alojamientos de datos.
+"""
+Tuplas -> tupla = ()
+
+Mostrar tupla -> 1. print(tupla) // 2.print(tupla[2:0]) // 3. x(1,2,3) , z,y,t = x , print(z,y,t) >> 1 2 3
+
+Contar cant. de veces que se repite un elemento -> print(x.count(2)) #Cuenta la cantidad de veces que hay un 2.
+
+Contar cant. de elementos -> print(len(x))
+
+Buscar posicion -> print(x.index(5)) #Busca el objeto 5 y devuelve su posicion // #Si no se encuentra devuelve ValueError.
+
+
+"""
