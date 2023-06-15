fe37f81e51cbb41b88c5d4cb13cbc22e910a7fea
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-15 20:03:44 -0300

    modifique el main

--- HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/main.py
+++ HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/main.py
@@ -4,12 +4,22 @@ import enemigos
 from enemigos import *
 from personajes import *
 
+# llamado de las clases localmente
+personaje = Guerrero()
+enemigo = Monstruos()
+
+
+def bienvenida_al_personaje():
+    print("\n-------------------------------------------------------------------------------------------")
+    print(f"HOLA {personaje.nombre.upper()}, BIENVENIDO A ESTA AVENTURA.")
+    print(
+        f"PARA COMENZAR RECUERDE QUE SU VIDA ES DE {personaje.vida} y su fuerza es {personaje.fuerza} .")
+    print(f"TENIENDO EN CUENTA ESO A LA HORA DE PELEAR SU FUERZA ES EL ATAQUE QUE INFLIGE AL ENEMIGO.")
+    print(f"¡ BUENA SUERTE !")
+
+
 # funcion de datos del guerrero
-
-"""
 def personaje_guerrero():
-    personaje = Guerrero()
-
     print("Digite los datos del guerrero:")
     nombre_personaje = input("Nombre: ")
     estatura_personaje = float(input("Estatura: "))
@@ -24,19 +34,21 @@ def personaje_guerrero():
     personaje.atributos()
 
 
+print("\n\t--------¡ JUEGO DE AVENTURA !--------\n")
+print("Usted sera un personaje y debera luchar contra un enemigo...\n")
 personaje_guerrero()
-"""
+bienvenida_al_personaje()
 # resto del main
+
 print("Digite contra quien desea pelear: ")
 opcion_pelea = input("Dragon(Drg) // Monstruo(Monst): ")
 change_minus = opcion_pelea.lower()  # converti en minuscula la opcion
 while change_minus == "monst" or change_minus == "drg":
     print("Dentro del bucle")
     if change_minus == "monst":
-        personaje = Guerrero()
         print(f"Guerrero {personaje.nombre}")
         print("Seleccionaste pelear con un Monstruo.\n")
-        enemigo = Monstruos()
+
         enemigo.atributos()
 
     elif change_minus == "drg":
