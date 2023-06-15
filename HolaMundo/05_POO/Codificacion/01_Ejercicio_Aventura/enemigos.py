df5c127bf62137d07609f7ff8e5a06aaf22976ec
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-15 16:55:40 -0300

    enemigos juego de aventura

---
+++ HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/enemigos.py
@@ -0,0 +1,19 @@
+# Clase enemigo
+import personajes
+from personajes import *
+
+
+class Monstruos:
+    def __init__(self):
+        self.nombre = "Eva"
+        self.vida = 800
+        self.defensa = 0
+        self.color = "Verde"
+    # metodo
+
+    def atributos(self):
+        print("---------------------------------------")
+        print(f"NOMBRE ENEMIGO: {self.nombre.upper()}")
+        print(f"VIDA: {self.vida}")
+        print(f"El color del enemigo es {self.color}")
+        print("---------------------------------------")
