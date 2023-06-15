717ced8600b684e8f66412ad5fd8564236254ea9
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-15 03:54:06 -0300

    explicacion de clases primera parte

---
+++ HolaMundo/Practica/Teoria/08_clases.py
@@ -0,0 +1,33 @@
+"""
+CLASES - OBJETOS
+Pilares basicos: 1.Herencia//2.Cohesión//3.Abstraccion//4.Polimorfismo//5.Acoplamiento//6.Encapsulamiento
+
+- Clase -> Diseño. (molde - plantilla)
+
+- Objeto -> Instancia de una clase --> Ejecucion ( MEMORIA )
+
+Componentes de una clase POO -> 1.Atributos // 2.Propiedades // 3. Metodos .
+1.Atributos -> 1.Los datos que manejo en una clase. // 2.Nombre y tipo de dato. // 3.Variable privada .
+2.Propiedades -> 1.Bloque de codigo {}, que definen el acceso a los atributos. // 2.Nombre y tipo de dato. // 3.get y set.
+3.Metodos -> 1.Bloques de codigo, publicos, que pueden tener parametros y definen el comportamiento de la Clase. //Funciones - Procedimientos
+Funcion -> retorna un valor // Procedimientos -> no retorna un valor
+
+Python en POO se comporta siguiendo estos pasos: 
+1.
+#Atributos de clase. -> Atributos que pertenecen a una clase
+class Usuario:
+    username = ""
+
+Usuario.username = "Franco"
+print(Usuario.username)
+
+2.
+#Atributos de instancia. 
+class Usuario:
+    username = ""
+
+user1 = Usuario()
+print(user1.username)
+
+"""
+
