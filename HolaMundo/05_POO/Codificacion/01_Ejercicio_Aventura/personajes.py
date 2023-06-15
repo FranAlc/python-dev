c1aff03674b1ee79aac4004281fc3e01a89b5863
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-15 16:55:53 -0300

    personaje juego de aventura

---
+++ HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/personajes.py
@@ -0,0 +1,27 @@
+# Clase personaje
+import enemigos
+from enemigos import *
+
+
+class Guerrero:
+    cant_golpes = 0
+    # Atributos de la clase Guerrero
+
+    def __init__(self):
+        self.nombre = ""
+        self.estatura = None
+        self.tipo_arma = ""
+        self.fuerza = None
+        self.vida = 1000
+
+    # Metodos
+    def atributos(self):
+        print("\nAtributos del guerrero: ")
+        print(f"Nombre del Guerrero: {self.nombre.title()}.")
+        print(f"Estatura: {self.estatura}")
+        print(f"Tipo de arma: {self.tipo_arma.title()}.")
+        print(f"FUERZA: {self.fuerza}")  # daño
+        print(f"VIDA: {self.vida}")
+
+    def atacar(self):
+        print(f"{self.nombre}, debes atacar")
