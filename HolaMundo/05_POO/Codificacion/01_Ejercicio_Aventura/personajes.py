f45a1792e05541315d5a4effd11ae33962f37911
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-16 02:03:25 -0300

    intentando desarrollar la pelea

--- HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/personajes.py
+++ HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/personajes.py
@@ -1,10 +1,10 @@
 # Clase personaje
-import enemigos
+import random
 from enemigos import *
 
 
 class Guerrero:
-    cant_golpes = 0
+
     # Atributos de la clase Guerrero
 
     def __init__(self):
@@ -12,7 +12,9 @@ class Guerrero:
         self.estatura = None
         self.tipo_arma = ""
         self.fuerza = None
-        self.vida = 1000
+        self.defensa = 200
+        self.vida = 2000
+        self.fallaste = 0
 
     # Metodos
     def atributos(self):
@@ -22,6 +24,28 @@ class Guerrero:
         print(f"Tipo de arma: {self.tipo_arma.title()}.")
         print(f"FUERZA: {self.fuerza}.")  # daño
         print(f"VIDA: {self.vida}.")
+        print(f"DEFENSA: {self.defensa}.")
 
     def atacar(self):
-        print(f"{self.nombre}, debes atacar")
+        acumulador = 0
+        opc = 1
+        aciertos = []
+        fallos = []
+        golpes = ["Golpeaste al Monstruo!".title(), "Fallaste".title()]
+        start = random.choice(golpes)
+        print(f"{self.nombre.title()}, debes atacar..\n")
+
+        while acumulador != 2000 and self.vida > 0:
+            if start == golpes[0]:
+                print("Lograste golpearlo !!".title())
+                acumulador += self.fuerza
+                aciertos.append(acumulador)
+                if sum(aciertos) == 2000:
+                    for i in aciertos:
+                        print(i)
+                    print("Derrotaste al monstruo".upper())
+
+            elif start == golpes[1]:
+                print("Fallaste !!".title())
+                acumulador += self.fallaste
+                fallos.append(acumulador)
