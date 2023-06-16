dc71a27ec296b1dc826f1f22f238d90cb04958ba
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-16 17:02:08 -0300

    Desarollo pelea pero sin el uso del daño y la vida

--- HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/pelea.py
+++ HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/pelea.py
@@ -7,13 +7,69 @@ personaje = Guerrero()
 
 
 class Pelea_Guerrero_Monstruo:
+
     def __init__(self):
-        self.lista_golpes = []
+        self.golpes_enemigo = []
+        self.golpes_guerrero = []
         self.golpes = ["ACERTO EL GOLPE !!", "FALLO EL GOLPE !!"]
-        self.pelea = ["Monstruo", "Guerrero"]
+        self.pelea = ["Guerrero", "Monstruo"]
         self.start = None
+        self.resultado = None
+        self.cant_golpes_guerrero = []
+        self.cant_golpes_enemigo = []
+        self.dato_final = "lograste vencer al monstruo !!"
+        self.dato_final2 = "fuiste derrotado!!"
 
     def combate(self):
         start = random.choice(self.pelea)
         print(f"Comienza atacando {start}")
-        
+        # comienza pelea guerrero
+        if start == self.pelea[0]:
+            for i in range(1, 9):
+                personaje.atacar()  # ataca el personaje
+                self.resultado = random.choice(self.golpes)
+                print(f"{self.resultado}\n")
+                self.golpes_guerrero.append(self.resultado)
+
+                enemigo.atacar()  # ataca el enemigo
+                self.resultado = random.choice(self.golpes)
+                print(f"{self.resultado}\n")
+                self.golpes_enemigo.append(self.resultado)
+        self.cant_golpes_guerrero = self.golpes_guerrero.count(
+            "ACERTO EL GOLPE !!")
+        self.cant_golpes_enemigo = self.golpes_enemigo.count(
+            "ACERTO EL GOLPE !!")
+        if self.cant_golpes_guerrero > self.cant_golpes_enemigo:
+            print(
+                f"CANTIDAD DE GOLPES QUE LE DISTE = {self.cant_golpes_guerrero}")
+            print(f"{self.dato_final.upper()}")
+        else:
+            print(
+                f"CANTIDAD DE GOLPES DADOS POR EL MONSTRUO = {self.cant_golpes_enemigo}")
+            print(f"{personaje.nombre} {self.dato_final2.upper()}")
+
+        # comienza pelea monstruo
+        if start == self.pelea[1]:
+            for i in range(1, 9):
+                enemigo.atacar()  # ataca el enemigo
+                self.resultado = random.choice(self.golpes)
+                print(f"{self.resultado}\n")
+                self.golpes_enemigo.append(self.resultado)
+
+                personaje.atacar()  # ataca el personaje
+                self.resultado = random.choice(self.golpes)
+                print(f"{self.resultado}\n")
+                self.golpes_guerrero.append(self.resultado)
+
+        self.cant_golpes_guerrero = self.golpes_guerrero.count(
+            "ACERTO EL GOLPE !!")
+        self.cant_golpes_enemigo = self.golpes_enemigo.count(
+            "ACERTO EL GOLPE !!")
+        if self.cant_golpes_guerrero > self.cant_golpes_enemigo:
+            print(
+                f"CANTIDAD DE GOLPES QUE LE DISTE = {self.cant_golpes_guerrero}")
+            print(f"{self.dato_final.upper()}")
+        else:
+            print(
+                f"CANTIDAD DE GOLPES DADOS POR EL MONSTRUO = {self.cant_golpes_enemigo}")
+            print(f"{personaje.nombre} {self.dato_final2.upper()}")
