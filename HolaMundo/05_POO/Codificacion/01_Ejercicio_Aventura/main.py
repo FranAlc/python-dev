ca48ac2d8019d7e47fb2a0819063ce12d28d2cca
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-16 02:03:50 -0300

    agregue la funcion pelea

--- HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/main.py
+++ HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/main.py
@@ -1,6 +1,5 @@
 # Main
-import personajes
-import enemigos
+import random
 from enemigos import *
 from personajes import *
 
@@ -11,7 +10,7 @@ enemigo = Monstruos()
 
 def bienvenida_al_personaje():
     print("\n-------------------------------------------------------------------------------------------")
-    print(f"HOLA {personaje.nombre.upper()}, BIENVENIDO A ESTA AVENTURA.")
+    print(f"\tHOLA {personaje.nombre.upper()}, BIENVENIDO A ESTA AVENTURA.")
     print(
         f"PARA COMENZAR RECUERDE QUE SU VIDA ES DE {personaje.vida} y su fuerza es {personaje.fuerza} .")
     print(f"TENIENDO EN CUENTA ESO A LA HORA DE PELEAR SU FUERZA ES EL ATAQUE QUE INFLIGE AL ENEMIGO.")
@@ -19,19 +18,32 @@ def bienvenida_al_personaje():
 
 
 # funcion de datos del guerrero
+
+
 def personaje_guerrero():
     print("Digite los datos del guerrero:")
     nombre_personaje = input("Nombre: ")
     estatura_personaje = float(input("Estatura: "))
     arma_personaje = input("Ingrese un arma: ")
-    fuerza_personaje = int(input("Fuerza: "))
 
     # alojar datos anteriores
     personaje.nombre = nombre_personaje
     personaje.estatura = estatura_personaje
     personaje.tipo_arma = arma_personaje
-    personaje.fuerza = fuerza_personaje
+    personaje.fuerza = 400
     personaje.atributos()
+
+# pelea
+
+
+def pelea():
+
+    # random
+    opciones = "personaje"
+    if opciones == "personaje":
+        personaje.atacar()
+    elif opciones == [1]:
+        enemigo.atacar()
 
 
 print("\n\t--------¡ JUEGO DE AVENTURA !--------\n")
@@ -44,12 +56,14 @@ print("Digite contra quien desea pelear: ")
 opcion_pelea = input("Dragon(Drg) // Monstruo(Monst): ")
 change_minus = opcion_pelea.lower()  # converti en minuscula la opcion
 while change_minus == "monst" or change_minus == "drg":
-    print("Dentro del bucle")
-    if change_minus == "monst":
-        print(f"Guerrero {personaje.nombre}")
-        print("Seleccionaste pelear con un Monstruo.\n")
 
+    if change_minus == "monst":
+        print(f"\nGuerrero {personaje.nombre}")
+        print("Seleccionaste pelear con un Monstruo.\n")
+        # Enemigo informacion
         enemigo.atributos()
+        # funcion pelea
+        pelea()
 
     elif change_minus == "drg":
         print("Todavia no desarrollado.")
