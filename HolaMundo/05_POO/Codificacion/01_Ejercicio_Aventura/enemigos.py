73f60316b3f13ef4d839622c75359888c4fb20f6
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-16 02:04:46 -0300

    falta corregir el metodo ataque en la clase

--- HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/enemigos.py
+++ HolaMundo/05_POO/Codificacion/01_Ejercicio_Aventura/enemigos.py
@@ -1,12 +1,12 @@
 # Clase enemigo
-import personajes
+
 from personajes import *
 
 
 class Monstruos:
     def __init__(self):
         self.nombre = "Eva"
-        self.vida = 800
+        self.vida = 2000
         self.defensa = 0
         self.color = "Verde"
     # metodo
@@ -17,3 +17,23 @@ class Monstruos:
         print(f"VIDA: {self.vida}.")
         print(f"El color del enemigo es {self.color}.")
         print("---------------------------------------")
+
+    def atacar(self):
+        acumulador = 0
+        aciertos = []
+        fallos = []
+        golpes = []
+        golpes.append("Golpeaste al Guerrero!")
+        golpes.append("Fallaste!")
+        start = random.choice(golpes)
+        if start == [0]:
+            acumulador += self.fuerza
+            aciertos.append(acumulador)
+            if sum(aciertos) == 2000:
+                print("Derrotaste al Guerrero!".title())
+
+        elif start == [1]:
+            acumulador += self.fallaste
+            fallos.append(acumulador)
+
+        print(f"{self.nombre.title()}, debes atacar..")
