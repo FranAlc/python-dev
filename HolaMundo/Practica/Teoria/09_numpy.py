15e00e9fb75340818070710df2a4d935a24aaac1
Franco Galan <alcaidefranco.07@gmail.com>
2023-06-21 03:53:49 -0300

    orientando aprendizaje hacia numpy

---
+++ HolaMundo/Practica/Teoria/09_numpy.py
@@ -0,0 +1,27 @@
+"""
+#Numpy -> import numpy as np -> "np" tomaria el uso de numpy por el resto del archivo
+
+1. Potente estructuras de datos.
+2. Implementa matrices y matrices multidimensionales.
+3. Estas estructuras garantizan calculos eficientes con matrices.
+
+#Matriz 2x2
+matriz = np.array([1,2],[3,4]) 
+>> [[1 2]
+    [3 4]]  
+
+#Matriz 2x3
+matriz = np.array([1,2,3],[4,5,6])
+>> [[1 2 3]
+    [4 5 6]]    
+
+Definir un vector utilizando una lista -> vector = [1,2,3]
+
+Definir un vector utilizando un array numpy -> vector = np.array([1,2,3])
+
+Definir un vector columna -> vector = np.array([1],[2],[3])
+
+
+
+
+"""
